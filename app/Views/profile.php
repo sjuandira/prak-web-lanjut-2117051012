371e3d6d6266700c785f9fe0121361b2c809fe0b
sjuandira <salsajuandira04@gmail.com>
2023-09-05 12:12:11 +0700

    Initial commit

---
+++ app/Views/profile.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- <title>Title of the document</title> -->
+</head>
+<body>
+
+<h1>Ini halaman profile</h1>
+<table>
+<tr>
+  <td>Nama</td>
+  <td>:</td>
+  <td><?=$nama ?></td>
+</tr>
+<tr>
+  <td>Kelas</td>
+  <td>:</td>
+  <td><?=$kelas ?></td>
+</tr>
+<tr>
+  <td>NPM</td>
+  <td>:</td>
+  <td><?=$npm ?></td>
+</tr>
+</table>
+</body>
+</html>
