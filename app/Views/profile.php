05a7d86d744faac49be9e7e58208e732360d5e5e
sjuandira <salsajuandira04@gmail.com>
2023-09-11 12:44:01 +0700

    Ganti Profile

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -2,26 +2,12 @@
 <html lang="en">
 <head>
   <!-- <title>Title of the document</title> -->
+  <link rel="stylesheet" href="<?php echo base_url('./style.css'); ?>">
 </head>
 <body>
-
-<h1>Ini halaman profile</h1>
-<table>
-<tr>
-  <td>Nama</td>
-  <td>:</td>
-  <td><?=$nama ?></td>
-</tr>
-<tr>
-  <td>Kelas</td>
-  <td>:</td>
-  <td><?=$kelas ?></td>
-</tr>
-<tr>
-  <td>NPM</td>
-  <td>:</td>
-  <td><?=$npm ?></td>
-</tr>
-</table>
+<div class="content">
+<img src="<?php echo base_url('./SalsabilaJuandira.jpg'); ?>" alt="">
+<h1>Salsabila Juandira</h1>
+<p>Perkenakalkan nama saya <?= $nama ?> dengan NPM <?= $npm ?> dari kelas <?= $kelas ?></p>
 </body>
 </html>
