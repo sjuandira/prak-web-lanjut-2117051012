b7b3a28485809bcf9299ec9534b24d0d3f22245c
sjuandira <salsajuandira04@gmail.com>
2023-10-24 09:52:25 +0700

    Update-Delete

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,4 +16,7 @@ $routes->get('/user/profile', [UserController::class,'profile']);
 $routes->get('/user/create', [UserController::class,'create']);
 $routes->post('/user/store', [UserController::class,'store']);
 $routes->get('/user', [UserController::class,'index']);
+$routes->get('user/(:any)/edit', [UserController::class, 'edit']);
+$routes->put('user/(:any)', [UserController::class, 'update']);
 $routes->get('user/(:any)', [UserController::class, 'show']);
+$routes->delete('user/(:any)', [UserController::class, 'destroy']);
--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\KelasModel;
 use App\Models\UserModel;
+use LDAP\Result;
 
 class UserController extends BaseController
 {   
@@ -66,6 +67,49 @@ class UserController extends BaseController
 
         return view('create_user', $data);
     }
+    public function edit($id){
+        $user = $this->userModel->getUser($id);
+        $kelas = $this->kelasModel->getKelas();
+
+        $data=[
+            'title'=> 'Edit User',
+            'user'=>$user,
+            'kelas'=> $kelas,
+        ];
+
+        // dd($data);
+
+        return view('edit_user', $data);
+
+    }
+    public function update($id){
+        $path= 'assets/uploads/img/';
+        $foto= $this->request->getFile('foto');
+
+        
+        $data= [
+            'nama'=> $this->request->getVar('nama'),
+            'id_kelas'=>$this->request->getVar('kelas'),
+            'npm'=> $this->request->getVar('npm'),
+            
+        ];
+        if($foto->isValid()){
+            $name= $foto->getRandomName();
+
+            if($foto->move($path, $name)){
+                $foto= base_url($path.$name);
+                $data['foto']= $foto;
+            }
+        }
+
+        $result= $this->userModel->updateUser($data, $id);
+
+        if(!$result){
+            return redirect()->back()->withInput()->with('error', 'Gagal Menyimpan Data');
+        }
+
+        return redirect()->to('/user');
+    }
     public function store(){
         $path = 'assets/uploads/img/';
         $foto =$this->request->getFile('foto');
@@ -115,5 +159,13 @@ class UserController extends BaseController
             'user' => $user,
         ];
         return view('profile', $data);
+    }
+    public function destroy($id){
+        $result= $this->userModel->deleteUser($id);
+        if(!$result){
+            return redirect()->back()->with('error', 'Gagal menghapus data');
+        }
+        return redirect()->to(base_url('/user'))
+        ->with('success','Berhasil menghapus data');
     }
 }
